80319e9637bcd2751f07f30e38283bc3f43be4c3
Bruno Graf <brunograf7@gmail.com>
2026-03-03 18:10:25 -0300

    Primeiros comandos
    Executei os primeiros comandos em Python. Também concluí os 3 primeiros desafios

---
+++ Desafios/D01.py
@@ -0,0 +1,4 @@
+print ('=====Desafio 01=====')
+print ('Qual é o seu nome?')
+nome = input()
+print ('Olá, ' + nome + '! Prazer em te conhecer!')
---
+++ Desafios/D03.py
@@ -0,0 +1,4 @@
+print ('=====Desafio 03=====')
+n1 = input('Digite um número: ')
+n2 = input('Digite outro número: ')
+print ('A soma entre eles é: ', int(n1) + int(n2))
