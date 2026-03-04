bf5ca78839ab97915ba5aa124ef6a527e9af38a5
Bruno Graf <brunograf7@gmail.com>
2026-03-04 17:21:27 -0300

    Tipos primitivos e saída de dados
    Ex05 - Pergunta números e exibe a soma
    Ex06 - Solicita uma mensagem e exibe os tipos primitivos

---
+++ Exercícios/Ex06.py
@@ -0,0 +1,2 @@
+n = input("Digite algo: ")
+print (f'É numérico? {n.isnumeric()}\nÉ alfabético? {n.isalpha()}\nÉ alfanumérico? {n.isalnum()}\nÉ minusculo? {n.islower()}\nÉ maiusculo? {n.isupper()}')
