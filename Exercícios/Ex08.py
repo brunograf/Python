0b7f416982572635c2f5b17fe3a1484fb8713b37
Bruno Graf <brunograf7@gmail.com>
2026-03-05 17:19:49 -0300

    Operadores aritméticos
    Ex07 e Ex08 - Como funcionam os operadores aritméticos

---
+++ Exercícios/Ex08.py
@@ -0,0 +1,3 @@
+n1 = int (input ('Digite um número: '))
+n2 = int (input ('Digite outro número: '))
+print (f'A soma vale {n1 + n2}\n A subtração vale {n1 - n2}\n A multiplicação vale {n1 * n2}\n A divisão vale {n1 / n2}\n A divisão inteira vale {n1 // n2}\n O resto da divisão vale {n1 % n2:.3f}\n A potência vale {n1 ** n2}')
