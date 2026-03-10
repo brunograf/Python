be14d9afc3e2a8062775664cd926af3d3b39831c
Bruno Graf <brunograf7@gmail.com>
2026-03-10 17:17:51 -0300

    Manipulando texto
    Operações com string: fatiamento, análise, transformações, junção e separação

---
+++ Exercícios/Ex12.py
@@ -0,0 +1,16 @@
+frase = 'Curso em Vídeo Python'
+print (frase[1:15])
+print (len(frase))
+print (frase.count('o'))
+print (frase.find('deo'))
+print (frase.replace('Python', 'Android'))
+print (frase.upper())
+print (frase.lower())
+print (frase.capitalize())
+print (frase.title())
+
+dividido = frase.split()
+print (dividido)
+print (dividido[2])
+print (dividido[2][3])
+print ('-'.join(dividido))
