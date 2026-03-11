cc9f0e82289dac84d00c17ed93278e1530f8faa6
Bruno Graf <brunograf7@gmail.com>
2026-03-11 17:47:12 -0300

    Cores no terminal
    Ex15 - Biblioteca de cores no terminal

---
+++ Exercícios/Ex15.py
@@ -0,0 +1,4 @@
+from colorama import Fore, Back, Style
+print(f'{Fore.RED} {Back.WHITE} {Style.BRIGHT}Erro')
+print(f'{Fore.GREEN} {Back.YELLOW} {Style.DIM}Sucesso')
+print(f'{Fore.YELLOW} {Back.BLACK} {Style.NORMAL}Atenção')
