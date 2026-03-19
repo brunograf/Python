02b50982393241d111d2039676bc765319c2c537
Bruno Graf <brunograf7@gmail.com>
2026-03-19 17:08:11 -0300

    Tuplas
    Ex23 - exibe uma tupla com lanches
    Ex24 - soma duas tuplas e mostra algumas informações

---
+++ Exercícios/Ex23.py
@@ -0,0 +1,9 @@
+lanche = ('hamburguer', 'suco', 'pizza', 'pudim')
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
+print('Comi pra caramba!')
+
+lanche = ('hamburguer', 'suco', 'pizza', 'pudim')
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posição {pos}')
+print('Comi pra caramba!')
