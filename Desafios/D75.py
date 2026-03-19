6e4896a93d4133a579c033d790e21fe2269c1caf
Bruno Graf <brunograf7@gmail.com>
2026-03-19 18:10:46 -0300

    Correção do exercício D75

--- Desafios/D75.py
+++ Desafios/D75.py
@@ -1,14 +1,13 @@
-par = 0
-num = []
-for c in range (0, 4):
-    valor = int(input('Digite um número: '))
-    num.append(valor)
-    if valor % 2 == 0:
-        par += 1
-valores = tuple(num)
-print (f'O número 9 apareceu {valores.count(9)} vezes.')
-if 3 in valores:
-    print (f'O primeiro número 3 foi digitado na posição {valores.index(3)+1}.')
+num = (int(input('Digite um número: ')),
+       int(input('Digite outro número: ')),
+       int(input('Digite mais um número: ')),
+       int(input('Digite um último número: ')))
+print (f'O número 9 apareceu {num.count(9)} vezes.')
+if 3 in num:
+    print (f'O primeiro número 3 foi digitado na posição {num.index(3)+1}.')
 else:
     print ('O número 3 não foi digitado.')
-print(f'Foram digitados {par} números pares.')
+print(f'Os valores pares digitados foram ', end='')
+for n in num:
+    if n % 2 == 0:
+        print (f'{n} ', end=' ')
