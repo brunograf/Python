88ccd6be0a60b8862719e6134fb24c168ea8b3d2
Bruno Graf <brunograf7@gmail.com>
2026-03-24 18:06:59 -0300

    Correção D78

--- Desafios/D78.py
+++ Desafios/D78.py
@@ -2,11 +2,12 @@ valores = []
 for c in range(0, 5):
     valores.append(int(input(f'Digite um valor para a posição {c}: ')))
 print(f'Os valores digitados foram {valores}')
-if valores.count(max(valores)) > 1:
-    print(f'Os maiores valores digitados foi {max(valores)} nas posições {valores.index(max(valores))}')
-else:
-    print(f'O maior valor digitado foi {max(valores)} na posição {valores.index(max(valores))}')
-if valores.count(min(valores)) > 1:
-    print(f'Os menores valores digitados foi {min(valores)} nas posições {valores.index(min(valores))}')
-else:
-    print(f'O menor valor digitado foi {min(valores)} na posição {valores.index(min(valores))}')
+print(f'O maior valor digitado foi {max(valores)} nas posições ', end='')
+for i, v in enumerate(valores):
+    if v == max(valores):
+        print(f'{i}... ', end=' ')
+print ()
+print(f'O menor valor digitado foi {min(valores)} nas posições ', end='')
+for i, v in enumerate(valores):
+    if v == min(valores):
+        print(f'{i}... ', end=' ')
