ddd1ba98cd431532abf0a9f51ff3d7a438f4324e
Bruno Graf <brunograf7@gmail.com>
2026-03-26 17:20:20 -0300

    Dicionários
    Ex31 - dicionário que exibe nome, sexo, idade e peso
    Ex32 - dicionário que mostra dois estados brasileiros
    Ex33 - solicita três UFs e suas siglas e exibe na tela

---
+++ Exercícios/Ex31.py
@@ -0,0 +1,4 @@
+pessoas = {'nome': 'Bruno', 'sexo': 'M', 'idade': 25}
+pessoas['peso'] = 75
+for k, v in pessoas.items():
+    print(f'{k} = {v}')
---
+++ Exercícios/Ex32.py
@@ -0,0 +1,8 @@
+estado1 = {'UF': 'Rio de Janeiro', 'Sigla': 'RJ'}
+estado2 = {'UF': 'Sao Paulo', 'Sigla': 'SP'}
+brasil = []
+brasil.append(estado1)
+brasil.append(estado2)
+print(brasil)
+print(brasil[0]['UF'])
+print(brasil[1]['Sigla'])
